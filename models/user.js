6a8fda193e46b7662652bb7ab8ec26c5441324b5
Lucas Emanoel <lucase@tuta.io>
2019-11-05 14:56:39 -0400

    initial user model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+});
+
+export default mongoose.model('User', userSchema);
